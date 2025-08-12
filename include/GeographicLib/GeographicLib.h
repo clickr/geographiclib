0be80f7ed5ed436f5a14086a76c464fc7fe8cc01
David Hart <me@davidhart.id.au>
2025-08-12 17:29:05 +0800

    Add bridging header to enable use in a Swift Package

---
+++ include/GeographicLib/GeographicLib.h
@@ -0,0 +1,42 @@
+//
+//  GeographicLib.h
+//  SwiftGeographicLib
+//
+//  Created by David Hart on 27/7/2025.
+//
+
+#if defined(SWIFT_PACKAGE)
+#include "GeographicLib/Accumulator.hpp"
+#include "GeographicLib/AuxAngle.hpp"
+#include "GeographicLib/AlbersEqualArea.hpp"
+#include "GeographicLib/AzimuthalEquidistant.hpp"
+#include "GeographicLib/CassiniSoldner.hpp"
+#include "GeographicLib/Constants.hpp"
+#include "GeographicLib/DMS.hpp"
+#include "GeographicLib/Ellipsoid.hpp"
+#include "GeographicLib/GARS.hpp"
+#include "GeographicLib/GeoCoords.hpp"
+#include "GeographicLib/Geodesic.hpp"
+#include "GeographicLib/GeodesicLine.hpp"
+#include "GeographicLib/Geohash.hpp"
+#include "GeographicLib/Geoid.hpp"
+#include "GeographicLib/Georef.hpp"
+#include "GeographicLib/Gnomonic.hpp"
+#include "GeographicLib/GravityCircle.hpp"
+#include "GeographicLib/GravityModel.hpp"
+#include "GeographicLib/Intersect.hpp"
+#include "GeographicLib/LambertConformalConic.hpp"
+#include "GeographicLib/LocalCartesian.hpp"
+#include "GeographicLib/MagneticCircle.hpp"
+#include "GeographicLib/MagneticModel.hpp"
+#include "GeographicLib/Math.hpp"
+#include "GeographicLib/MGRS.hpp"
+#include "GeographicLib/NearestNeighbor.hpp"
+#include "GeographicLib/OSGB.hpp"
+#include "GeographicLib/PolarStereographic.hpp"
+#include "GeographicLib/PolygonArea.hpp"
+#include "GeographicLib/Rhumb.hpp"
+#include "GeographicLib/SphericalHarmonic2.hpp"
+#include "GeographicLib/TransverseMercator.hpp"
+#include "GeographicLib/Utility.hpp"
+#endif
